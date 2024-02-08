d28e0252801b4ff6c89a4a14141fd371feda7d4e
R Chauhan <active023@Rs-MacBook-Pro.lan>
2024-02-08 17:36:44 -0600

    added gitignore file

--- Hashmap/src/FirstUniqueCharacter.java
+++ Hashmap/src/FirstUniqueCharacter.java
@@ -1,2 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstUniqueCharacter {
+    //LC387
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, -1);
+            } else {
+                map.put(c, i);
+            }
+        }
+
+        int idx = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> keySet : map.entrySet()) {
+            if (keySet.getValue() != -1) {
+                idx = Math.min(keySet.getValue(), idx);
+            }
+        }
+        return idx == Integer.MAX_VALUE ? -1 : idx;
+    }
+
 }
--- Introduction/src/ReplaceArrayElements.java
+++ Introduction/src/ReplaceArrayElements.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ReplaceArrayElements {
+import java.util.Arrays;
+
+public class ReplaceArrayElements {
+
+    public static void main(String[] args) {
+        int[] arr = {8, 9, 5, 11, 6, 1, 7, 6};
+        new ReplaceArrayElements().replace(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void replace(int[] arr) {
+        int max = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] > max) {
+                max = arr[i];
+                arr[i] = -1;
+            } else {
+                arr[i] = max;
+            }
+        }
+    }
 }
