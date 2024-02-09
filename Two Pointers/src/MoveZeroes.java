44437a3badfc26d8a343f1f63baef3a02befbae1
active90 <chauhan.raman06@gmail.com>
2024-02-09 00:34:45 -0600

    solved more problems

--- Two Pointers/src/MoveZeroes.java
+++ Two Pointers/src/MoveZeroes.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MoveZeroes {
+public class MoveZeroes {
+    public static void main(String[] args) {
+
+    }
+
+    public void moveZeroes(int[] nums) {
+        if (nums.length <= 1) {
+            return;
+        }
+        int nonZero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (nonZero != i) {
+                    nums[nonZero] = nums[i];
+                    nums[i] = 0;
+                }
+
+                nonZero++;
+            }
+        }
+    }
 }
